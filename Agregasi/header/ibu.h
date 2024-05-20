fd18261ecbf7e3a3346f2b629c2b01ef73b9d2d0
mhmdfakhrim <muhfakhri4@gmail.com>
2024-05-20 15:01:39 +0700

    commit

--- Agregasi/header/ibu.h
+++ Agregasi/header/ibu.h
@@ -2,5 +2,16 @@
 #define IBU_H
 
 class ibu {
-    
-}
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
