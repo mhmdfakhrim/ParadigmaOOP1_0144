e0de796fafc7537d5147c8dc5132dd3553450a30
mhmdfakhrim <muhfakhri4@gmail.com>
2024-05-20 20:12:29 +0700

    commit

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -17,3 +17,28 @@ class orang {
             return a + b;
         }
 };
+
+class pelajar : public orang {
+    public: 
+        string sekolah;
+
+        pelajar(string pNama, string pSekolah): orang(pNama), sekolah(pSekolah) {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+
+        ~pelajar() {
+            cout << "Pelajar dihapus\n" << endl;
+        }
+
+        string perkenalan() {
+            return "Halo, nama saya " + nama + " dari sekolah " + sekolah + "\n \n";
+        }
+};
+
+int main() {
+    pelajar siswa1("Andi Aksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
