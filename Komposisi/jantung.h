f0ffcb17a5ca5928890baaa1c7a80f15f4542d26
mhmdfakhrim <muhfakhri4@gmail.com>
2024-05-20 15:44:14 +0700

    commit jantung

---
+++ Komposisi/jantung.h
@@ -0,0 +1,15 @@
+#infndef JANTUNG_H
+#define JANTUNG_H
+
+using namespacestd;
+
+class jantung {
+    public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
